e2cb828f399f686d3606293cc426d59be8f4cd97
Dave G <dpg3k@virginia.edu>
2026-04-06 07:57:47 -0400

    Add support for deletes

--- uvaaptsdao/delete.go
+++ uvaaptsdao/delete.go
@@ -4,16 +4,49 @@
 
 package uvaaptsdao
 
-import (
-//"log"
-
-// postgres
-// _ "github.com/lib/pq"
-)
-
 //
 // delete methods
 //
+
+func (dao *Dao) DeleteBagsBySubmission(sid string) error {
+
+	stmt, err := dao.Prepare("DELETE FROM bags WHERE submission = $1")
+	if err != nil {
+		return err
+	}
+	defer stmt.Close()
+	return execPrepared(stmt, sid)
+}
+
+func (dao *Dao) DeleteFilesBySubmission(sid string) error {
+
+	stmt, err := dao.Prepare("DELETE FROM files WHERE submission = $1")
+	if err != nil {
+		return err
+	}
+	defer stmt.Close()
+	return execPrepared(stmt, sid)
+}
+
+func (dao *Dao) DeleteFailuresBySubmission(sid string) error {
+
+	stmt, err := dao.Prepare("DELETE FROM submission_failures WHERE submission = $1")
+	if err != nil {
+		return err
+	}
+	defer stmt.Close()
+	return execPrepared(stmt, sid)
+}
+
+func (dao *Dao) DeleteConflictsBySubmission(sid string) error {
+
+	stmt, err := dao.Prepare("DELETE FROM submission_conflicts WHERE submission = $1")
+	if err != nil {
+		return err
+	}
+	defer stmt.Close()
+	return execPrepared(stmt, sid)
+}
 
 //
 // end of file
--- uvaaptsdao/insert.go
+++ uvaaptsdao/insert.go
@@ -22,13 +22,13 @@ var initialBagState = "registered"
 func (dao *Dao) AddSubmission(sid string, cid string, collection string, storage string) error {
 
 	// insert into submissions
-	stmt1, err := dao.Prepare("INSERT INTO submissions( identifier, client, collection_name, storage ) VALUES( $1,$2, $3, $4 )")
+	stmt, err := dao.Prepare("INSERT INTO submissions( identifier, client, collection_name, storage ) VALUES( $1,$2, $3, $4 )")
 	if err != nil {
 		return err
 	}
-	defer stmt1.Close()
+	defer stmt.Close()
 
-	err = execPrepared(stmt1, sid, cid, collection, storage)
+	err = execPrepared(stmt, sid, cid, collection, storage)
 	if err != nil {
 		return err
 	}
@@ -40,12 +40,12 @@ func (dao *Dao) AddSubmission(sid string, cid string, collection string, storage
 func (dao *Dao) AddBag(bagName string, sid string) error {
 
 	// insert into bags
-	stmt1, err := dao.Prepare("INSERT INTO bags( name, submission ) VALUES( $1,$2 )")
+	stmt, err := dao.Prepare("INSERT INTO bags( name, submission ) VALUES( $1,$2 )")
 	if err != nil {
 		return err
 	}
-	defer stmt1.Close()
-	err = execPrepared(stmt1, bagName, sid)
+	defer stmt.Close()
+	err = execPrepared(stmt, bagName, sid)
 	if err != nil {
 		return err
 	}
@@ -56,48 +56,48 @@ func (dao *Dao) AddBag(bagName string, sid string) error {
 func (dao *Dao) AddFile(fileName string, hash string, sid string, bagName string) error {
 
 	// insert into files
-	stmt1, err := dao.Prepare("INSERT INTO files( name, hash, submission, bag_name ) VALUES( $1,$2, $3, $4 )")
+	stmt, err := dao.Prepare("INSERT INTO files( name, hash, submission, bag_name ) VALUES( $1,$2, $3, $4 )")
 	if err != nil {
 		return err
 	}
-	defer stmt1.Close()
-	return execPrepared(stmt1, fileName, hash, sid, bagName)
+	defer stmt.Close()
+	return execPrepared(stmt, fileName, hash, sid, bagName)
 }
 
 // AddApproval -- add a new approval with the specified attributes
 func (dao *Dao) AddApproval(sid string, who string) error {
 
 	// insert into files
-	stmt1, err := dao.Prepare("INSERT INTO approvals( submission, who ) VALUES( $1,$2 )")
+	stmt, err := dao.Prepare("INSERT INTO approvals( submission, who ) VALUES( $1,$2 )")
 	if err != nil {
 		return err
 	}
-	defer stmt1.Close()
-	return execPrepared(stmt1, sid, who)
+	defer stmt.Close()
+	return execPrepared(stmt, sid, who)
 }
 
 // AddConflict -- add a new conflict with the specified attributes
 func (dao *Dao) AddConflict(sid string, newFileId int64, basis string, conflictFileId int64) error {
 
 	// insert into files
-	stmt1, err := dao.Prepare("INSERT INTO submission_conflicts( submission, new_file, basis, conflicting_file ) VALUES( $1,$2, $3, $4 )")
+	stmt, err := dao.Prepare("INSERT INTO submission_conflicts( submission, new_file, basis, conflicting_file ) VALUES( $1,$2, $3, $4 )")
 	if err != nil {
 		return err
 	}
-	defer stmt1.Close()
-	return execPrepared(stmt1, sid, newFileId, basis, conflictFileId)
+	defer stmt.Close()
+	return execPrepared(stmt, sid, newFileId, basis, conflictFileId)
 }
 
 // AddFailure -- add a new failure with the specified attributes
 func (dao *Dao) AddFailure(sid string, reason string) error {
 
 	// insert into files
-	stmt1, err := dao.Prepare("INSERT INTO submission_failures( submission, failure ) VALUES( $1,$2 )")
+	stmt, err := dao.Prepare("INSERT INTO submission_failures( submission, failure ) VALUES( $1,$2 )")
 	if err != nil {
 		return err
 	}
-	defer stmt1.Close()
-	return execPrepared(stmt1, sid, reason)
+	defer stmt.Close()
+	return execPrepared(stmt, sid, reason)
 }
 
 //
